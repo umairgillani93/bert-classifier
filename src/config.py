fb6e90cdcef3b4acf60ad132bdb32698452d4642
umairgillani93 <umairgillani93@gmail.com>
2022-11-13 22:17:05 +0500

    BERT custom fine-tuning

---
+++ src/config.py
@@ -0,0 +1,15 @@
+import os
+import transformers
+
+DEVICE = 'cpu'
+MAX_LEN = 64
+TRAIN_BATCH_SIZE = 8
+VALID_BATCH_SIZE = 4
+EPOCHS = 5
+BERT_PATH = os.getenv('HOME') + '/models/bert_model/model'
+MODEL_PATH = os.getenv('HOME') + '/checkpoints/'
+TRAINING_FILE = os.getenv('HOME') + '/datasets/fake-news/train.csv'
+TOKENIZER = transformers.BertTokenizer.from_pretrained(os.getenv('HOME') + '/models/bert_model/tokenizer')
+TRAIN_PATH = os.getenv('HOME') + '/datasets/fake_news/train.csv'
+TEST_PATH = os.getenv('HOME') + '/datasets/fake_news/test.csv'
+
---
+++ src/dataset.py
@@ -0,0 +1,53 @@
+import os
+import sys
+import torch
+import config
+import pandas as pd 
+
+class BertDataset:
+    def __init__(self):
+        self.train_df = pd.read_csv(config.TRAIN_PATH)
+        self.cols = ['text', 'label']
+        self.train_df.drop([col for col in self.train_df.columns if \
+                                col not in self.cols], axis=1, inplace=True)
+
+        #print(self.train_df.columns)
+        self.text = self.train_df.text
+        self.label = self.train_df.label
+        self.tokenizer = config.TOKENIZER
+        self.max_len = config.MAX_LEN
+        #print('everything done')
+
+    def __len__(self):
+        return len(self.train_df.text)
+
+    def __getitem__(self, idx):
+        text = str(self.train_df.text[idx])
+        text = " ".join(text.split())
+        
+        # inputs from tokenizer
+        inputs = self.tokenizer.encode_plus(text,
+                                            None,
+                                            max_length=self.max_len,
+                                            pad_to_max_length=True)
+
+        print(f'\nType: {type(inputs)}')
+        print(f'\ninput keys: {inputs.keys()}')
+
+        ids = inputs['input_ids']
+        mask = inputs['attention_mask']
+        token_type_ids = inputs['token_type_ids']
+
+        # conver all the above parameters to tensors and return as a dictionary
+        return {
+                'ids': torch.tensor(ids, dtype=torch.long),
+                'mask': torch.tensor(mask, dtype=torch.long),
+                'token_type_ids': torch.tensor(token_type_ids, dtype=torch.long),
+                'targets': torch.tensor(self.label[idx], dtype=torch.float)
+                }
+
+
+
+#bd = BertDataset()
+#print(bd.__getitem__(2))
+
